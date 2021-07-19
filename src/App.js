e4139f5ef2b316a861b4dde755ebaeaec1530dd6
NirabHossain <nirabamth@gmail.com>
2021-07-19 16:37:02 +0600

    debugged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
